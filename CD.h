9b68c6d3d7c2d3a639486368814005a0967968ff
farixco <farodriguezor@correo.url.edu.gt>
2023-09-27 10:45:21 -0600

    Constructor CD definido.

--- CD.h
+++ CD.h
@@ -2,11 +2,27 @@
 #include "Song.h"
 #include "List.h"
 #include <string>
+#include <fstream>
+#include <stdexcept>
 
-class CD : public List<Song> {
+class CD : public List<Song*> {
+   std::string Name;
   public:
    int IndexOf(Song cancion);
-   CD(std::string directoryPath) {
+   std::string GetName();
+   CD(std::string filePath) {
       // código para leer el archivo, más que todo revisión de errores y utilizar Add() para agregarlos a la lista
+      std::string line;
+      std::ifstream file(filePath);
+      if (file.is_open()) {
+	 while (std::getline(file, line)) {
+	    Song* temp = new Song(line, Name);
+	    this->Add(temp);
+	    delete temp;
+	 }
+	 file.close();
+      } else {
+	 throw std::invalid_argument("Archivo no encontrado o no es de texto.");
+      }
    }
 };
