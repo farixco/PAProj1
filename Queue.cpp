e8da9509ba0b0c9068e9a4fd7d47eae252afc317
farixco <farodriguezor@correo.url.edu.gt>
2023-09-28 12:28:58 -0600

    Método Queue::Reorder implementado.

--- Queue.cpp
+++ Queue.cpp
@@ -1,6 +1,7 @@
 #include "Queue.h"
 #include <stdexcept>
 #include "Song.h"
+#include <algorithm>
 
 template <typename T>
 void Queue<T>::Enqueue(T item) {
@@ -21,14 +22,57 @@ T Queue<T>::Dequeue() {
 
 // método especializado solo para ordenar canciones
 template <>
-void Queue<Song*>::Reorder(int type) {
+void Queue<Song*>::Reorder(int type, int order) {
+   //  0: artista, 1: canción, 2: duración || 0 descendiente, 1 ascendiente
+   int count = Count();
+   List<Song*>* listTmp = new List;
+   for (int i = 0; i < count; ++i) {
+      listTmp->Add(Dequeue());
+   }
    if (type == 0) {
-      // implementar más tarde
+      // utilizando std::sort, función anónima para poder compararlos
+      if (order == 0) {
+	 std::sort(0, listTmp->Count(), [](const Song* lhs, const Song* rhs) {
+	    return lhs->Artist > rhs->Artist;
+	 });
+      } else if (order == 1) {
+	 std::sort(0, listTmp->Count(), [](const Song* lhs, const Song* rhs) {
+	    return lhs->Artist < rhs->Artist;
+	 });
+      } else {
+	 throw std::invalid_argument("Orden de ordenamiento inválido.");
+      }
    } else if (type == 1) {
-      // implementar más tarde
+      // utilizando std::sort, función anónima para poder compararlos
+      if (order == 0) {
+	 std::sort(0, listTmp->Count(), [](const Song* lhs, const Song* rhs) {
+	    return lhs->Name > rhs->Name;
+	 });
+      } else if (order == 1) {
+	 std::sort(0, listTmp->Count(), [](const Song* lhs, const Song* rhs) {
+	    return lhs->Name < rhs->Name;
+	 });
+      } else {
+	 throw std::invalid_argument("Orden de ordenamiento inválido.");
+      }
    } else if (type == 2) {
-      // implementar más tarde
+      // utilizando std::sort, función anónima para poder compararlos
+      if (order == 0) {
+	 std::sort(0, listTmp->Count(), [](const Song* lhs, const Song* rhs) {
+	    return lhs->Duration > rhs->Duration;
+	 });
+      } else if (order == 1) {
+	 std::sort(0, listTmp->Count(), [](const Song* lhs, const Song* rhs) {
+	    return lhs->Duration < rhs->Duration;
+	 });
+      } else {
+	 throw std::invalid_argument("Orden de ordenamiento inválido.");
+      }
    } else {
       throw std::invalid_argument("Tipo de reordenamiento inválido.");
    }
+   for (int i = 0; i < count; ++i) {
+      Enqueue(listTmp->GetAt(i));
+   }
+   delete listTmp;
 }
--- Queue.h
+++ Queue.h
@@ -10,6 +10,6 @@ class Queue : private List<T> {
    using List<T>::IndexOf;
    void Enqueue(T item);
    T Dequeue();
-   // el método para reordenar recibe un int por el tipo de reordenamiento que hace. 0 es por artista, 1 es por canción y 2 es por duración
-   void Reorder(int type);
+   // el método para reordenar recibe un int por el tipo de reordenamiento que hace. 0 es por artista, 1 es por canción y 2 es por duración. el int order recibe 0 para descendiente y 1 para ascendente.
+   void Reorder(int type, int order);
 };
