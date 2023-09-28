e55f7ee9881e6615b5922bc7d56b37c989c7e9bc
farixco <farodriguezor@correo.url.edu.gt>
2023-09-27 22:17:04 -0600

    Queue<T>::Dequeue definido. Algunos métodos hechos públicos.

--- Queue.cpp
+++ Queue.cpp
@@ -1,7 +1,19 @@
 #include "Queue.h"
+#include <stdexcept>
 
 template <typename T>
 void Queue<T>::Enqueue(T item) {
-   this->Add(item);
+   Add(item);
 }
 
+template <typename T>
+T Queue<T>::Dequeue() {
+   if (Count() > 0) {
+   T item;
+   item = this->Head->Data;
+   this->RemoveAt(0);
+   return item;
+   } else {
+      throw std::range_error("La cola no tiene elementos.");
+   }
+}
--- Queue.h
+++ Queue.h
@@ -5,6 +5,9 @@ template <typename T>
 class Queue : private List<T> {
    // construída sobre la lista, pero solo permite acceso de ciertas maneras;
   public:
+   using List<T>::Count;
+   using List<T>::GetAt;
+   using List<T>::IndexOf;
    void Enqueue(T item);
    T Dequeue();
 };
