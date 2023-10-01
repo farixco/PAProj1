f7a46023732bd3bf57e461b91c5b6b8cef701fa2
farixco <farodriguezor@correo.url.edu.gt>
2023-10-01 10:15:20 -0600

    Main inicializado. CMake definido como sistema de compilación.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <filesystem>
+#include <ncurses.h>
+#include "../include/CD.h"
+#include "../include/List.h"
+
+using std::cout, std::endl, std::string;
+
+int main(int argc, char *argv[]) {
+   initscr();
+   endwin();
+   return 0;
+}
